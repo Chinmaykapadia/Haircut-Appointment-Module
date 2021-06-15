927a34913116be01132f7a84058fade81a0c045d
Chinmaykapadia <chinmay.tagline@gmail.com>
2021-06-15 23:04:11 +0530

    status changes on 15/06/21

--- src/app/add-status/add-status.component.ts
+++ src/app/add-status/add-status.component.ts
@@ -6,14 +6,21 @@ import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
   styleUrls: ['./add-status.component.css']
 })
 export class AddStatusComponent implements OnInit {
+
   status = [ "i'm Available" , "i'm not Available" , "i'm in Meeting" ];
   form: FormGroup;
   submitted: boolean;
   addControls: any;
+  statusValue: string;
+  clientName_value: string;
+  selectedOption: any;
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.form = this.fb.group({
+      select_status: [''],
+      client_name: [''],
       addStatusForm: this.fb.array([
         this.getControls()
       ])
@@ -30,10 +37,37 @@ export class AddStatusComponent implements OnInit {
       meeting: [''],
     })
   }
+
+  getFormArray(): FormArray{
+    return this.form.get('addStatusForm') as FormArray;
+  }
   onAdd(){
+    this.statusValue = this.selectedOption ;
+    console.log(this.statusValue);
+    console.log(this.clientName_value);
+    
+    
     this.submitted = true;
     this.addControls = <FormArray> this.form.controls['addStatusForm'];
     this.addControls.push(this.getControls());
+    console.log(this.form.controls['addStatusForm'].value.clientName);
+    
+    // this.getFormArray.setValue([
+    // {
+    //     clientName : this.clientName_value,
+    //     status : this.statusValue
+    // }]
+    // );
+  }
+
+  onAvailable(index: number){
+    this.statusValue = 'Available';
+  }
+  onNotAvailable(){
+
+  }
+  onInMeeting(){
+
   }
   removeItem(index: number){
     const control = <FormArray>this.form.controls["addStatusForm"];
