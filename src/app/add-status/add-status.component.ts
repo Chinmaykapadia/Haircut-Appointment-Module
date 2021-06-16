d132c43068f057b4f86d417a12297aaf2d1eda55
Chinmaykapadia <chinmay.tagline@gmail.com>
2021-06-16 22:52:54 +0530

    edit,delete,update changes 16/06/21

--- src/app/add-status/add-status.component.ts
+++ src/app/add-status/add-status.component.ts
@@ -6,7 +6,7 @@ import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
   styleUrls: ['./add-status.component.css']
 })
 export class AddStatusComponent implements OnInit {
-  @Input() stat: string;
+
   status = [ "i'm Available" , "i'm not Available" , "i'm in Meeting" ];
   form: FormGroup;
   submitted: boolean;
@@ -14,19 +14,19 @@ export class AddStatusComponent implements OnInit {
   clientName_value: string;
   selectedOption: any;
   formArray = [];
-  buttonValue = [ 'Available' , 'Not Available' , 'In Meeting' ];
-  clientV: any;
-  stats: any;
+  buttonValue = [ "i'm Available" , "i'm not Available" , "i'm in Meeting" ];
+  buttonName: string;
+  findId: number;
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.form = this.fb.group({
       select_status: [''],
       client_name: [''],
-      clname: [''],
-      stat: ['']
     });
     this.submitted = false;
+    this.buttonName = 'Add';
   }
 
   onAdd(){
@@ -34,11 +34,24 @@ export class AddStatusComponent implements OnInit {
     this.submitted = true;
     console.log(this.selectedOption);
     console.log(this.clientName_value);
-    
-    this.formArray.push(this.form.value);  
-    console.log(this.formArray);
+
+    if(this.buttonName == 'Add'){
+      this.formArray.push(this.form.value);  
+      console.log(this.formArray);
+      this.form.reset();
+    }
+    else{
+      console.log("Updated");
+      this.formArray[this.findId].select_status = this.selectedOption;
+      this.formArray[this.findId].client_name = this.clientName_value;
+      this.form.reset();
+      this.buttonName = 'Add';
+      console.log(this.formArray);
+      
+    }
+
   }
-  onBtnClick(i_: number, index: number){
+  onStatusBtnClick(i_: number, index: number){
     console.log(this.buttonValue[index]);
     // this.clientV = this.buttonValue[index];
     console.log(this.formArray[index]);
@@ -46,28 +59,41 @@ export class AddStatusComponent implements OnInit {
     this.formArray[i_].select_status = this.buttonValue[index];
     console.log(this.formArray[index]);
     console.log(this.formArray);
-    
-    
     // this.stats = this.formArray[index].select_status;
   }
+
   Delete(){
+    this.formArray.splice(this.findId,1);
+    this.form.reset();
+    this.buttonName = 'Add';
+    console.log(this.formArray);
+    
    // this.formArray.splice(i_);
   }
+
   updateData(i_: number){
     this.form.patchValue({
       select_status: this.formArray[i_].select_status,
       client_name: this.formArray[i_].client_name,
-    })
-    //this.formArray[i_].select_status
+    });
+   // this.addToedit = true;
+    this.buttonName = 'Update';
+    this.findId = i_;
+    console.log("find id:---=>",this.findId);
+    
   }
 
   deleteData(i_: number){
-    this.formArray.splice(i_);
+
+    console.log(i_);
+    this.formArray.splice(i_,1);
+    console.log(this.formArray);
+    
   }
 
   removeItem(index: number){
+
     console.log(index);
-    
     this.formArray.splice(0,index);
   }
 
